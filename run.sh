22efe3fac1d531f7355c61055f77ed12f3a9da75
mpochron <7736131+mpochron@users.noreply.github.com>
2018-10-08 12:18:16 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
+
+if [ ! -f "/etc/ssh/ssh_host_rsa_key" ]; then
+	# generate fresh rsa key
+	ssh-keygen -f /etc/ssh/ssh_host_rsa_key -N '' -t rsa
+fi
+if [ ! -f "/etc/ssh/ssh_host_dsa_key" ]; then
+	# generate fresh dsa key
+	ssh-keygen -f /etc/ssh/ssh_host_dsa_key -N '' -t dsa
+fi
+
 if [ "${AUTHORIZED_KEYS}" != "**None**" ]; then
     echo "=> Found authorized keys"
     mkdir -p /user/.ssh
